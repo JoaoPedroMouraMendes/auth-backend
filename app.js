7fadef36a0cc4d23181863669dd3a7c97e16763f
João Pedro <joaopedromouramendes1@gmail.com>
2024-01-24 12:25:46 -0300

    Configurações para o deploy na vercel

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const connectDB = require("./src/db/connect");
 connectDB();
 
 //* Configurações
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
